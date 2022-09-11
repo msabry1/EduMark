7d79045569563f13fa2479bc2e39c8bcc81ec576
Python-Egypt <msabrym2004@gmail.com>
2022-09-11 16:48:18 -0700

    Add Templates And static files

---
+++ edu/contact/views.py
@@ -0,0 +1,6 @@
+import re
+from django.shortcuts import render
+
+# Create your views here.
+def contact(request):
+    return render(request,"contact.html")
---
+++ edu/courses/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def courses(request):
+    return render(request,"courses.html")
